bbd28fad56f8202adfd485c78ddeeb490716b577
MAgungArdiansyah <55588249+MAgungArdiansyah@users.noreply.github.com>
2021-02-15 14:46:01 +0700

    add first code & comments

---
+++ Basic/bin/Basic.dart
@@ -0,0 +1,6 @@
+import 'package:Basic/Basic.dart' as Basic;
+
+void main(List<String> arguments) {
+  print('Hello world: ${Basic.calculate()}!');
+
+}
---
+++ Basic/bin/Comments.dart
@@ -0,0 +1,9 @@
+void main(List<String> arguments){
+  // single line comments
+  /*
+    multi
+    line
+    comment
+   */
+
+}
