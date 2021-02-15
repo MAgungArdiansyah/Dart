ff5391797130347be68b8d14e0e9d32db6943c8d
MAgungArdiansyah <55588249+MAgungArdiansyah@users.noreply.github.com>
2021-02-15 15:30:26 +0700

    add variable

---
+++ Belajar Dart/bin/Variable.dart
@@ -0,0 +1,17 @@
+void main(){
+  // Variable dapat di deklarasikan langsung isinya atau setelahnya
+  String greetings = 'Hello Agung!';
+  print(greetings);
+  print('');
+
+  /*
+    dart mendukung type inference dengan menggunakan 'var' dart akan
+    secara otomatis menentukan tipe datanya
+   */
+  var myAge;
+  myAge = 20;
+  print(myAge);
+
+  // int
+
+}
